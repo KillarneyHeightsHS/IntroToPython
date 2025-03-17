54018d6734d0122ee9b1437b9f87c488a7ec406f
mr-bev <aaron.beverley1@det.nsw.edu.au>
2025-03-17 22:42:50 +0000

    getting indoor code to work

--- src/function_variables/indoor.py
+++ src/function_variables/indoor.py
@@ -1,6 +1,6 @@
 def indoor():
     # This code is an example and should be replaced with your own solution
-    value = input()
+    value = input("What is the word? ")
     print(value.lower())
 
 

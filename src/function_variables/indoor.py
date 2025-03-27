d43c465cf83c92b05414e0ebae2663bb1bbb79af
Annie20080920 <juyeong.cheun444@education.nsw.gov.au>
2025-03-27 01:34:27 +0000

    message

--- src/function_variables/indoor.py
+++ src/function_variables/indoor.py
@@ -1,7 +1,8 @@
 def indoor():
     # This code is an example and should be replaced with your own solution
-    value = input("what is the world? ")
-    print(value.lower())
+    print("Hello")
+    print("This is CS50")
+    print(50)
 
 
 if __name__ == "__main__":
